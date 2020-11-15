dfec1326c2dbe8abc8e07eff7fd57e8c543cb25d
sami jaghouar <sami.jaghouar@polynom.io>
2020-11-15 17:05:58 +0100

    add reset game button

--- client/flask/src/run.py
+++ client/flask/src/run.py
@@ -17,7 +17,7 @@ app = Flask(__name__,
 
 @app.route('/')
 def home():
-    return render_template('home.html',my_host = my_host)
+    return render_template('home.html',my_host = my_host, api_host=api_host)
 
 
 
